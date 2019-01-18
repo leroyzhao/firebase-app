fc0a37382ed8e8d9d106a3a32938f0afc26e8d20
leroyzhao <leroyzhao99@gmail.com>
2019-01-18 11:44:41 -0500

    add projectlist to dashboard

---
+++ src/components/project/ProjectSummary.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ProjectSummary = () => {
+  return (
+    <div className="card z-depth-4 project-summary">
+      <div className="card-content grey-text text-darken-3">
+        <span className="crd-title">Project Title</span>
+        <p>Posted by LZ</p>
+        <p className="grey-text">Jan 18, 11:30am</p>
+      </div>
+  </div>
+  )
+}
+
+export default ProjectSummary
