d2011dd5448b761b215e936b6fe46c12ec5ef50b
leroyzhao <leroyzhao99@gmail.com>
2019-01-21 11:32:15 -0500

    subscribe project components to store with dummy data

--- src/components/project/ProjectSummary.js
+++ src/components/project/ProjectSummary.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const ProjectSummary = () => {
+const ProjectSummary = ({project}) => {
   return (
     <div className="card z-depth-4 project-summary">
       <div className="card-content grey-text text-darken-3">
-        <span className="crd-title">Project Title</span>
-        <p>Posted by LZ</p>
+        <span className="crd-title">{project.title}</span>
+        <p>{project.content}, Posted by LZ</p>
         <p className="grey-text">Jan 18, 11:30am</p>
       </div>
   </div>
--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,4 +1,10 @@
-const initState = {}
+const initState = {
+  projects: [
+    {id: '1', title: 'abc', content: 'blah blahblahblah blah'},
+    {id: '2', title: 'def', content: 'blah blahblahblah blah'},
+    {id: '3', title: 'xyz', content: 'blah blahblahblah blah'},
+  ]
+}
 
 const projectReducer = (state = initState, action) => {
   return state
